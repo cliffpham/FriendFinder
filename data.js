2517c43ac415fa70ec66e6182deb3e056c0cd5e2
cliffpham <cliffpham@gmail.com>
2018-06-03 23:17:42 -0700

    polished up files

---
+++ data.js
@@ -0,0 +1,63 @@
+var cryptidArray = [
+    {
+        name: 'Bigfoot',
+        photo: 'https://media.giphy.com/media/l1KdcZQd7LhIjz2Q8/giphy.gif',
+        answers: [
+            '1',
+            '2',
+            '5',
+            '3',
+            '1',
+            '2',
+            '3',
+            '5',
+            '1',
+            '1'
+        ]},
+        {name: 'Nessie',
+        photo: 'https://media.giphy.com/media/3xz2BYdD9uuttbYuWc/giphy.gif',
+        answers: [
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1'
+        ]},
+        {name: 'Jersey Devil',
+        photo: 'https://media.giphy.com/media/vvWKmZhu0vDnlxQTRj/giphy.gif',
+        answers: [
+            '3',
+            '2',
+            '3',
+            '4',
+            '4',
+            '4',
+            '3',
+            '5',
+            '4',
+            '1'
+        ]},
+        {name: 'Jackalope',
+        photo: 'https://media.giphy.com/media/26FxMw9e4UG4pGja8/giphy.gif',
+        answers: [
+            '3',
+            '5',
+            '2',
+            '5',
+            '5',
+            '2',
+            '5',
+            '5',
+            '4',
+            '5'
+        ]}
+
+];
+
+
+module.exports = cryptidArray;
--- server.js
+++ server.js
@@ -8,39 +8,7 @@ var PORT = 3000
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
  
-var friendArray = [
-    {
-        name: 'Bigfoot',
-        photo: 'https://media.giphy.com/media/l1KdcZQd7LhIjz2Q8/giphy.gif',
-        answers: [
-            '1',
-            '2',
-            '5'
-        ]},
-        {name: 'Nessie',
-        photo: 'https://media.giphy.com/media/3xz2BYdD9uuttbYuWc/giphy.gif',
-        answers: [
-            '1',
-            '1',
-            '1'
-        ]},
-        {name: 'Jersey Devil',
-        photo: 'https://media.giphy.com/media/vvWKmZhu0vDnlxQTRj/giphy.gif',
-        answers: [
-            '3',
-            '2',
-            '3'
-        ]},
-        {name: 'Jackalope',
-        photo: 'https://media.giphy.com/media/26FxMw9e4UG4pGja8/giphy.gif',
-        answers: [
-            '5',
-            '5',
-            '4'
-        ]}
-
-];
-
+var cryptids = require('./data.js');
 var userdataArray = [];
 
 
@@ -77,17 +45,17 @@ app.post('/survey',function(req,res){
     var matchImage = '';
     var totalDifference = 1000; //number gets replaced during comparison, starting with a large number
 
-    for (var i = 0; i < friendArray.length; i++){
-        // console.log(friendArray[i].answers);
+    for (var i = 0; i < cryptids.length; i++){
+        // console.log(cryptids[i].answers);
         var diff = 0;
 
-        // console.log(friendArray[i].answers);
+        // console.log(cryptids[i].answers);
         
     for(var j=0; j < userResponse.length; j++) {
 
         
         // Math.abs does not differentiate between positive and negativew integers
-        diff += Math.abs(friendArray[i].answers[j] - userResponse[j]);     
+        diff += Math.abs(cryptids[i].answers[j] - userResponse[j]);     
       
         }
   
@@ -97,12 +65,12 @@ app.post('/survey',function(req,res){
 
         if (diff < totalDifference) {
             console.log('Closest match = ' + diff);
-            console.log('Friend name = ' + friendArray[i].name);
-            console.log('Friend image = ' + friendArray[i].photo);
+            console.log('Friend name = ' + cryptids[i].name);
+            console.log('Friend image = ' + cryptids[i].photo);
 
             totalDifference = diff;
-            matchName = friendArray[i].name;
-            matchImage = friendArray[i].photo;
+            matchName = cryptids[i].name;
+            matchImage = cryptids[i].photo;
         }
 
     }
